a898bcc0bd1c921fdd4b988b0a9512924c043a65
“ijasz” <jass07rtr@gmail.com>
2023-07-18 19:30:48 +0530

    first page created

---
+++ lib/firstpage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Firstpage extends StatelessWidget {
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(body: Text('WELCOME TO FIRST PAGE'),);
+  }
+}
