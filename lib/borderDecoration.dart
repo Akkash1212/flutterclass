fb93a5dce40ef59a4b418e37f0995f458cb2ea0c
“ijasz” <jass07rtr@gmail.com>
2023-07-25 20:02:52 +0530

    Create Border and Make Decoration

---
+++ lib/borderDecoration.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Mycointainer extends StatelessWidget {
+  const Mycointainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 150,
+          width: 150,
+          margin: EdgeInsets.all(15),
+          // margin: EdgeInsets.only(left: 10, right: 25),
+          // margin: EdgeInsets.symmetric(horizontal: 40),
+
+          alignment: Alignment.center,
+          color: Colors.brown,
+          child: Text('Flutter'),
+        ),
+        Container(
+          height: 150,
+          width: 150,
+          alignment: Alignment.topCenter,
+          decoration: BoxDecoration(
+            color: Colors.tealAccent,
+            border: Border.all(width: 10, color: Colors.red),
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.8),
+                blurRadius: 5,
+                offset: Offset(15, 15),
+              ),
+            ],
+          ),
+          child: Text('flutter'),
+        ),
+      ],
+    );
+  }
+}
