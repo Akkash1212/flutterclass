3e69413e8e59d1d60f4956f7bb2a1cf842a5ed54
“ijasz” <jass07rtr@gmail.com>
2023-07-27 20:10:03 +0530

    patturns

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'borderDecoration.dart';
 import 'colum_row.dart';
+import 'liftpyramid.dart';
+import 'pyramid.dart';
+import 'userpage.dart';
 
 void main() {
   runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home());
+    return MaterialApp(home: User());
   }
 }
